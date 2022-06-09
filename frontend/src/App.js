9902c7bef7717ace83672a449998f888407b7b6e
shravanatirtha <shravz.ceo@gmail.com>
2022-06-09 19:49:23 +0530

    formatted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,16 @@ import { Button, Heading, Stack, Text } from "@chakra-ui/react";
 function App() {
   return (
     <Stack spacing={4} bg="white" p={8} borderRadius="lg">
-    <Heading as="h1" size="md" color="brand.900">
-      Learning Chakra UI
-    </Heading>
-    <Text as="p" fontSize="md" color="brand.500">
-      Your first Chakra components:
-    </Text>
-    <Button variantColor="brand" isFullWidth>
-      Click me
-    </Button>
-  </Stack>
+      <Heading as="h1" size="md" color="brand.900">
+        Learning Chakra UI
+      </Heading>
+      <Text as="p" fontSize="md" color="brand.500">
+        Your first Chakra components:
+      </Text>
+      <Button variantColor="brand" isFullWidth>
+        Click me
+      </Button>
+    </Stack>
   );
 }
 
