a5c9c8ee0bdd29eba2d4bf2ceebc997aecb09515
shravanatirtha <shravz.ceo@gmail.com>
2022-06-11 21:21:50 +0530

    profile component

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,81 @@
+import Icon from "@chakra-ui/icon";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { useMediaQuery } from "@chakra-ui/media-query";
+import React from "react";
+import { BsChatDotsFill } from "react-icons/bs";
+import { DiCodeigniter, DiWebplatform } from "react-icons/di";
+function Profile() {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+  return (
+    <Flex
+      direction={isNotSmallerScreen ? "row" : "column"}
+      w="100%"
+      maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}
+    >
+      <Box alignSelf="center" px="16" py="16">
+        {/* <Heading fontWeight="extrabold" color="cyan.500" size="lg">
+          7+
+        </Heading> */}
+        {/* <Text fontSize="2xl" color="gray.400">
+          Years of Experience
+        </Text> */}
+      </Box>
+      <Box alignSelf="center" px="32" py="16">
+        <Text fontWeight="bold" fontSize="2xl">
+          Projects
+        </Text>
+        <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8}>
+          <Flex
+            rounded="xl"
+            direction="column"
+            mt={4}
+            bg="blue.400"
+            h="30vh"
+            w="30vh"
+            justify="flex-end"
+          >
+            <Icon color="white" p="4" as={BsChatDotsFill} w="24" h="24" />
+            <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+              Conversational AI
+            </Text>
+          </Flex>
+          <Flex
+            rounded="xl"
+            direction="column"
+            mt={4}
+            ml={isNotSmallerScreen ? 4 : 0}
+            bg="gray.100"
+            h="30vh"
+            w="30vh"
+            justify="flex-end"
+            _hover={{ bg: "teal.400" }}
+          >
+            <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24" />
+            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+              Sketched
+            </Text>
+          </Flex>
+          <Flex
+            rounded="xl"
+            direction="column"
+            mt={4}
+            ml={isNotSmallerScreen ? 4 : 0}
+            bg="gray.100"
+            h="30vh"
+            w="30vh"
+            justify="flex-end"
+            _hover={{ bg: "green.400" }}
+          >
+            <Icon as={DiWebplatform} p="4" w="24" h="24" color="black" />
+            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+              Mailit
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+}
+
+export default Profile;
