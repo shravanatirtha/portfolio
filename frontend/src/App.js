0e52feac7e41250d8d7f8edbe501ce2e4ade5407
shravanatirtha <shravz.ceo@gmail.com>
2022-06-11 21:23:08 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,52 @@
-import { Button, Heading, Stack, Text } from "@chakra-ui/react";
-
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, Heading, Spacer, VStack } from "@chakra-ui/layout";
+import { FaGithub, FaGoogle, FaLinkedin, FaMoon, FaSun } from "react-icons/fa";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Logo from "./img/stc.png";
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <Stack spacing={4} bg="white" p={8} borderRadius="lg">
-      <Heading as="h1" size="md" color="brand.900">
-        Learning Chakra UI
-      </Heading>
-      <Text as="p" fontSize="md" color="brand.500">
-        Your first Chakra components:
-      </Text>
-      <Button variantColor="brand" isFullWidth>
-        Click me
-      </Button>
-    </Stack>
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+          <image src={Logo} />
+        </Heading>
+
+        <Spacer></Spacer>
+        <IconButton
+          icon={<FaLinkedin />}
+          isRound="true"
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/shravanatirtha/")
+          }
+        ></IconButton>
+
+        <IconButton
+          ml={2}
+          icon={<FaGithub />}
+          isRound="true"
+          onClick={() => window.open("https://github.com/shravanatirtha")}
+        ></IconButton>
+        <IconButton
+          ml={2}
+          icon={<FaGoogle />}
+          isRound="true"
+          onClick={() => window.open("https://g.dev")}
+        ></IconButton>
+        <IconButton
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+      </Flex>
+      <Header></Header>
+      <Profile></Profile>
+    </VStack>
   );
 }
 
