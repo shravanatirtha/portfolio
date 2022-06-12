a208373366ba8f9296c76219ac929c592ae897a8
shravanatirtha <shravz.ceo@gmail.com>
2022-06-12 19:01:48 +0530

    deploy code

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Shravana Tirtha");
-});
+const path = require("path");
+// -----------deployment----------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+} else {
+  app.get("/", (req, res) => {
+    res.send("Shravana Tirtha");
+  });
+}
+// -----------deployment----------
+
 app.listen(5000, () => {
   console.log("Node server running at http://localhost:5000");
 });
