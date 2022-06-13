7320a0c0245606e887059786c9b71f1496270c52
shravanatirtha <shravz.ceo@gmail.com>
2022-06-13 19:31:38 +0530

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 // -----------deployment----------
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
